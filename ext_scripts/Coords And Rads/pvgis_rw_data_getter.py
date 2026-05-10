c3f79cd411259d060b014fdcd2dda76099051005
Exzircon <mikaelfossli.work@gmail.com>
2026-05-10 16:13:20 +0200

    Fixed bug in pvgis_rw_data_getter.py

--- ext_scripts/Coords And Rads/pvgis_rw_data_getter.py
+++ ext_scripts/Coords And Rads/pvgis_rw_data_getter.py
@@ -1,18 +1,8 @@
-from pvgis_api import PVGISClient, RadiationDatabase
+from pvgis_api import PVGISClient, RadiationDatabase, OutputFormat
 import os
 
 # Create a client
 client = PVGISClient()
-
-""" # SARAH3 - Europe, Africa, parts of Asia (2005-2020)
-result = client.hourly_radiation(
-    year=2023,
-    lat=59.13342, lon=10.174096,
-    peakpower=1.0, loss=0,
-    radiation_db=RadiationDatabase.SARAH3,
-    output_format="csv",
-    optimal_angles=True
-) """
 
 INSCHOOL = "SandefjordSkoler.txt"
 INCOORDS = "SandefjordSkolerCoords.txt"
@@ -28,20 +18,27 @@ with open(INCOORDS, "r", encoding="utf-8") as file:
         address, lat, lon = line.strip().split(';')
         coords.append([address, float(lat), float(lon)])
 
+params = {
+    'lat': 0, #Lat and lon to be overwritten later
+    'lon': 0,
+    'year': 2023, #2023 is newest available data
+    'pvcalculation': 1, #Needed to get power calculation
+    'peakpower': 1, #Set to one so we can scale it in Godot
+    'loss': 0, #Set to zero so we can scale it in Godot
+    'components': 0, #Unsused
+    'raddatabase': RadiationDatabase.SARAH3, #Database for Europe
+    'outputformat': OutputFormat.CSV,
+    'optimalangles': 1 #Automatically optizime solar panel angle and azimuth
+}
+
+
 
 for i in range(len(schools)):
     print(f"Getting radiation data for {schools[i]} at coords {coords[i]}...")
-    result = client.hourly_radiation(
-        year=2023,
-        lat=coords[i][1], lon=coords[i][2],
-        peakpower=1.0, loss=0,
-        radiation_db=RadiationDatabase.SARAH3,
-        output_format="csv",
-        optimal_angles=True
-    )
-
+    params["lat"] = coords[i][1]
+    params["lon"] = coords[i][2]
+    result = client._make_request("seriescalc", params)
     os.makedirs('./radiation', exist_ok=True)
     
     with open(f'./radiation/{schools[i]}.csv', 'w',  newline="") as f:
         f.write(result['data'])
-
